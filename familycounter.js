474623ce1153d9a387b4a5505c440f1980329c2d
Vexilonius <35983623+Vexilonius@users.noreply.github.com>
2019-07-24 21:25:44 -0700

    Update familycounter.js

--- familycounter.js
+++ familycounter.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login();
+client.login(process.env.CLIENT_TOKEN);
 
 client.on("guildMemberAdd", member => {
     let saccharine = client.guilds.find(g => g.id === "538114152716173312");
