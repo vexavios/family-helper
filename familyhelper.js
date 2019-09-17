e6880e0d58407db4a4996f733a98c62856a6c589
Vexilonius <35983623+Vexilonius@users.noreply.github.com>
2019-09-17 11:36:58 -0700

    Update familyhelper.js

--- familyhelper.js
+++ familyhelper.js
@@ -7,14 +7,14 @@ client.on("ready", () => {
     console.log("family helper is now online!");
     client.user.setActivity("for people to help...", { type: "WATCHING" }).catch(console.error);
 });
- 
+
 // suggestions channel clearer
 client.on("message", message => {
     let server = client.guilds.find(g => g.id === "622821305133039616");
     let sugg_channel = server.channels.find(c => c.id === "622842780074377241");
     
     if (message.guild !== server && message.channel !== sugg_channel) return;
-    if (message.trim().toLowerCase().startsWith("s!suggest")) return;
+    if (message.content.trim().toLowerCase().startsWith("s!suggest")) return;
     
     message.delete()
         .then(msg => message.channel.send(message.author + "\n**꒰<a:eMessageHeart:623058074524975114>꒱ please suggest using the following format:**\n\n`s!suggest [suggestion]`"))
