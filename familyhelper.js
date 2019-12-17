4f1c03cee2e8d8d7d0040e2ed690c35b8bb166c5
Vexilon <35983623+Vexilon@users.noreply.github.com>
2019-12-16 16:10:55 -0800

    Update familyhelper.js

--- familyhelper.js
+++ familyhelper.js
@@ -39,7 +39,7 @@ client.on("message", message => {
 
         // determine if user is either ID or mention, and save it based on that, and save all command arguments
         if (message.mentions.members.first() && args.split(" ")[0].trim().substring(3, args.split(" ")[0].trim().length - 1) === message.mentions.members.first().toString().substring(2, message.mentions.members.first().toString().length - 1) && args.split(" ").slice(1).join(" ").trim() || !isNaN(parseInt(args.split(" ")[0])) && args.split(" ").slice(1).join(" ").trim()) {
-            if (args.split(" ")[0].trim().substring(3, args.split(" ")[0].trim().length - 1) === message.mentions.members.first().toString().substring(2, message.mentions.members.first().toString().length - 1)) messageUser = message.mentions.members.first();
+            if (message.mentions.members.first() && args.split(" ")[0].trim().substring(3, args.split(" ")[0].trim().length - 1) === message.mentions.members.first().toString().substring(2, message.mentions.members.first().toString().length - 1)) messageUser = message.mentions.members.first();
             else messageUser = args.split(" ")[0].trim();
 
             sendMessage = args.split(" ").slice(1).join(" ").trim();
@@ -47,7 +47,7 @@ client.on("message", message => {
             return message.channel.send(message.author + "\n**Usage:**```" + prefix + "send [Member mention or ID of user who you want to send the message to via DMs] [The message content that you want to DM to the user]```");
         }
 
-        if (args.split(" ")[0].trim().substring(3, args.split(" ")[0].trim().length - 1) === message.mentions.members.first().toString().substring(2, message.mentions.members.first().toString().length - 1)) {
+        if (message.mentions.members.first() && args.split(" ")[0].trim().substring(3, args.split(" ")[0].trim().length - 1) === message.mentions.members.first().toString().substring(2, message.mentions.members.first().toString().length - 1)) {
             messageUser.send({ embed: {
                 title: "Estella Secret Santa",
                 color: 16757940,
